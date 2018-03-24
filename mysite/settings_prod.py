e5952b21e875290d8d2a21df4e8917d489cbb780
ilgiz <hopeofkg@gmail.com>
2018-03-24 20:53:35 +0600

    settings all

--- mysite/settings_prod.py
+++ mysite/settings_prod.py
@@ -4,8 +4,8 @@ DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
        'NAME': 'simpledjango',
-       'USER': 'daniel',
-       'PASSWORD': '123dani123',
+       'USER': 'david',
+       'PASSWORD': 'david123',
        'HOST': 'localhost',
        'PORT': '',
    }
